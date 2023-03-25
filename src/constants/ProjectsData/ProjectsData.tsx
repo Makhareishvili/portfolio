dd14684c8aec21f722c97a9098018723b0ad4e95
Makhareishvili <levanimakharei7li@gmail.com>
2023-03-25 15:03:17 +0400

    add new project and update projectsData file

--- src/constants/ProjectsData/ProjectsData.tsx
+++ src/constants/ProjectsData/ProjectsData.tsx
@@ -2,6 +2,7 @@ import gptPic from "../../assets/photos/GPT-3.png";
 import fullMoonImage from "../../assets/photos/Full-moon.png";
 import ticTac from "../../assets/photos/ticTac.png";
 import toDoApp from "../../assets/photos/todoapp.png";
+import weatherApp from "../../assets/photos/weather-app.png";
 export interface IDATA {
   link: string;
   pic: string;
@@ -30,6 +31,15 @@ export const DATA: IDATA[] = [
     techList: ["TypeScript", "React", "CSS"],
   },
   {
+    link: "https://makhareishvili.github.io/react-weather-app/",
+    pic: weatherApp,
+    name: "Weather Forecast",
+    header: "App",
+    content:
+      "The Weather Forecast App is the most accurate weather app you need to navigate any weather conditions such as floods, hurricanes, tornadoes, snow, extreme wind, sun and rain. The Weather Forecast app has multiple detailed metrics like temperature, humidity, pressure, snow, clouds, rain and wind.",
+    techList: ["React", "CSS"],
+  },
+  {
     link: "https://makhareishvili.github.io/to-do-list-app/",
     pic: toDoApp,
     name: "To-do",
@@ -44,7 +54,7 @@ export const DATA: IDATA[] = [
     name: "Tic tac toe",
     header: "Game",
     content:
-      "a game in which two players seek in alternate turns to complete a row, a column, or a diagonal with either three O's or three X's drawn in the spaces of a grid of nine squares.",
+      "A game in which two players seek in alternate turns to complete a row, a column, or a diagonal with either three O's or three X's drawn in the spaces of a grid of nine squares.",
     techList: ["TypeScript", "React", "CSS"],
   },
 ];
